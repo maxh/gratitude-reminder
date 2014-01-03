b82fe6dbc0a5dec28514038d62b5b3d43681c97a
Max Heinritz <maxheinritz@gmail.com>
2014-01-02 22:57:07 -0800

    refactor html, initial form response server method

--- gratitude.py
+++ gratitude.py
@@ -14,9 +14,17 @@ class MainPage(webapp2.RequestHandler):
 
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('index.html')
-
         self.response.write(template.render())
+
+
+class SignupFormSubmission(webapp2.RequestHandler):
+
+    def post(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(self.request)
+
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/signup-form', SignupFormSubmission)
 ], debug=True)
