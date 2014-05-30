92986c5a3cb678b6fadb936e9549942b9b228bcd
Max Heinritz <maxheinritz@gmail.com>
2014-05-29 21:17:59 -0700

    Add error page; refactor main router; remove background color

--- web.py
+++ web.py
@@ -33,24 +33,32 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
+        not_registered_urls = {
+            'sign_up_link': users.create_login_url('/register'),
+            'sign_in_link': users.create_login_url('/'),
+        }
+        if self.request.get('sup') == 'error':
+            template = JINJA_ENVIRONMENT.get_template('error.html')
+            self.response.write(template.render())
+            return
+        # User is not logged in.
         user = users.get_current_user()
         if not user:
             template = JINJA_ENVIRONMENT.get_template('not_registered.html')
-            self.response.write(template.render(
-                sign_up_link=users.create_login_url('/register'),
-                sign_in_link=users.create_login_url('/')))
+            self.response.write(template.render(not_registered_urls))
             return
+        # User _just_ registered and may not be in the DB yet.
         if (self.request.get('sup') == 'just_registered'):
             template = JINJA_ENVIRONMENT.get_template('just_registered.html')
             self.response.write(template.render(user_email=user.email()))
             return
         users_db = models.User.query(models.User.email == user.email()).fetch(1)
+        # User is logged in but not signed up.
         if len(users_db) == 0:
             template = JINJA_ENVIRONMENT.get_template('not_registered.html')
-            self.response.write(template.render(
-                sign_up_link=users.create_login_url('/register'),
-                sign_in_link=users.create_login_url('/')))
+            self.response.write(template.render(not_registered_urls))
             return
+        # User is logged in and registered.
         user_db = users_db[0]
         status = 'registered'
         template = JINJA_ENVIRONMENT.get_template('registered.html')
@@ -68,9 +76,9 @@ class MainPage(webapp2.RequestHandler):
 class Register(webapp2.RequestHandler):
 
     def get(self):
-        user = users.get_current_user()
-        email = user.email()
         try:
+            user = users.get_current_user()
+            email = user.email()
             if len(models.User.query(models.User.email == email).fetch(1)) == 1:
                 self.redirect("/?sup=already_registered")
                 return
@@ -79,8 +87,11 @@ class Register(webapp2.RequestHandler):
             user.put()
             mail.send_email_from_template(email, file_id, 'welcome')
         except Exception as e:
+            logging.exception('Exception while registering:')
+            logging.exception(email)
             logging.exception(e)
-        self.redirect("/?sup=just_registered")
+            self.redirect('/?sup=error')
+        self.redirect('/?sup=just_registered')
 
 
 class Unsubscribe(webapp2.RequestHandler):
@@ -103,8 +114,11 @@ class Unsubscribe(webapp2.RequestHandler):
             user.key.delete()
             mail.send_email_from_template(email, file_id, 'unsubscribe')
         except Exception as e:
+            logging.exception('Exception while unsubscribing:')
+            logging.exception(email)
             logging.exception(e)
-        self.response.write("Successfully unsubscribed.")
+            self.redirect('/?sup=error')
+        self.response.write('Successfully unsubscribed.')
 
 
 routes = [
